834e19c93b4582c0e740a3d819ff7c7c1c184405
feb_16 <codingninjaslcfeb16@gmail.com>
2023-09-17 10:54:47 +0530

    start-server

--- index.js
+++ index.js
@@ -1,76 +1,14 @@
-//read a file
+const express = require('express');
+require('dotenv').config();
 
-// const fs = require('fs');
-const fs = require('fs/promises');
-const path = require('path');
+const app = express();
 
-const readFilePath = path.join(__dirname,'template.html');
-const writeFilePath = path.join(__dirname,'copy.html');
-//1
-// fs.readFile(readFilePath,'utf-8',function(err,data){
-//     if(err){
-//         console.log('error',err);
-//         return;
-//     }
-//     console.log('data',data);
-//     //write
-//     fs.writeFile(writeFilePath,data,function(err,data){
-//         if(err){
-//             console.log('error',err);
-//             return;
-//         }
-//         console.log('data copied succesfully');
-//     })
+const PORT = 8000;
 
-// })
-//2nd way promises
-// fs.readFile(readFilePath,'utf-8')
-//     .then((data) => {
-//         return fs.writeFile(writeFilePath,data)
-//     }).
-//     then((data) =>{
-//         console.log('copied succesfully');
-//     }).catch((err)=>{
-//         console.log('err',err);
-//     })
-let replaceData = {
-    first_name:'nikki',
-    last_name:'abc',
-    email:'n1@gmail.com'   
-};
-
-async function readAndWrite(){
-    try {
-        let data = await fs.readFile(readFilePath,'utf-8');
-        data = stringReplace(data);
-        await fs.writeFile(writeFilePath,data);
-        console.log('data -copied succesfully');
-    } catch (error) {
-        console.log('err',error);
+app.listen(PORT,function listenServer(err){
+    if(err){
+        console.log(`error is ${err}`);
+        return;
     }
-    
-}
-
-function stringReplace(data){
-    //keys 
-    //for in 
-    let keys = Object.keys(replaceData); ['first_name','last_name','email']
-    keys.map((item) =>{
-        let placeHolderName = `<%=${item.toUpperCase()}%>`;
-        data = data.replace(placeHolderName,replaceData[item]);
-    })
-    return data;
-}
-readAndWrite();
-
-console.log('i am last line');
-
-
-// let data = [1,2,3];
-// for(let i = 0;i< data.length;i++){
-//     data[i] = data[i]*2;
-// }
-
-// data.map((item,index)=>{
-// RETURN ITEM*2;
-// })
+    console.log(`server is up at port ${PORT} on ${process.env.enviroment}`);
+});
